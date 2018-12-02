a47932fabd002bc9f07a710ead709c77f0ed6be0
atamuhiuldin <atamuhiuldin@gmail.com>
2018-12-02 17:16:42 +0500

    Updates

---
+++ precache-manifest.01f325e9fe7d7a889cae380c33948ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2bb9f21a9164104feeaf",
+    "url": "/static/js/main.2bb9f21a.chunk.js"
+  },
+  {
+    "revision": "05fac6afbd28783ab23c",
+    "url": "/static/js/1.05fac6af.chunk.js"
+  },
+  {
+    "revision": "2bb9f21a9164104feeaf",
+    "url": "/static/css/main.4c48ae2f.chunk.css"
+  },
+  {
+    "revision": "3a9afef191002eb71650d8d11ebb82c0",
+    "url": "/index.html"
+  }
+];
